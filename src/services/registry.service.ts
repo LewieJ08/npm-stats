a00896c7e1301f7d925994f3c2765635ddc5e0b7
LewieJ08 <lewiej08@gmail.com>
2026-02-12 22:09:08 +0000

    Add npm registry service setup

---
+++ src/services/registry.service.ts
@@ -0,0 +1,30 @@
+import { RegistryInfo } from "./registry.types";
+
+export class RegistryService {
+    private readonly baseUrl: string;
+
+    constructor() {
+        this.baseUrl = 'https://registry.npmjs.org'
+    }
+
+    private async request<T>(path: string, options: RequestInit): Promise<T> {
+        const response = await fetch(`${this.baseUrl}${path}`, {
+            ...options,
+            ...options.headers
+        })
+
+        const data = await response.json();
+
+        if (!response.ok) {
+            throw new Error(`${data}`)
+        }
+
+        return data as T;
+    }
+
+    public getInfo(pkg: string): Promise<RegistryInfo> {
+        return this.request<RegistryInfo>(`/${pkg}`, {
+            method: 'GET'
+        })
+    }
+}
